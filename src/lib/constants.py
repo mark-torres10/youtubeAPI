eb683ce0c66fb4b2eceae07e180a9d35144b73ac
mark-torres10 <markptorres1@gmail.com>
2023-09-16 23:38:42 +0800

    nit updates

--- src/lib/constants.py
+++ src/lib/constants.py
@@ -4,5 +4,4 @@ YOUTUBE_CHANNEL_NAME = "Andrew Huberman"
 YOUTUBE_CHANNEL_HANDLE = "hubermanlab"
 YOUTUBE_CHANNEL_ID = "UC2D2CMWXMOVWx7giW1n3LIg"  # YouTube ID of Huberman podcast
 SPOTIFY_CHANNEL_ID = ""  # Spotify ID of Huberman podcast
-SQLITE_ENGINE = "sqlite:///youtube_data.db"
 CURRENT_SYNCTIMESTAMP = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
